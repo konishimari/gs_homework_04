f3f4eba08bcb70b01696ce11c0155022cf5720f1
konishimari <mari.konishi.works@gmail.com>
2023-08-06 13:11:04 +0900

    作成しました

---
+++ src/pages/news.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import style from '../styles/text.module.scss'
+
+
+const news = () => {
+    return (
+        <div className="text-5xl underline">
+        news
+        <p className= {style.test}>テスト</p>
+        </div>
+    );
+};
+
+export default news;
